3591cb52715d80f13470ba91a551d2e20a799437
Maik Pankow <maikpankow@gmail.com>
2026-01-26 20:46:01 +0100

    merch conflict resolved

--- javascript/animation.js
+++ javascript/animation.js
@@ -1,14 +1,9 @@
 window.addEventListener("load", () => {
   setTimeout(() => {
     document.body.classList.add("loaded");
-<<<<<<< HEAD
-  }, 600);
-});
-=======
   }, 100);
 });
 
 function goToSignup() {
   window.location.href = "./subpages/regist.html";
 }
->>>>>>> 4fc860bae99c7cba6ee88539c47a349eb2f70a3c
